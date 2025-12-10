6fc8d5d3c6cec0a64ab39d0d4baeb711a8fcb030
abhinav0411 <abhinavrai0419@gmail.com>
2025-12-11 03:18:33 +0530

    Day 7 done

---
+++ day6/day6_part2.py
@@ -0,0 +1,11 @@
+from day6_part1 import problem_list
+
+def solve():
+    last = len(problem_list) - 1
+    answer_list = [1 if x == "*" else 0 for x in problem_list[last]]
+    
+    for i in range()
+
+if __name__ == "__main__":
+    answer = solve()
+    print(answer)
